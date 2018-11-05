cd57b0d6df5a8007a3b9b6088fb538701304af98
Chris Lawrence <chris.lawrence@lexisnexis.co.uk>
2018-11-05 21:58:52 +0000

    added new controller

---
+++ Controllers/SecretController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretApi.Controllers
+{
+    [Route("api/[controller]")]
+    // ^^ Syntactic sugar to replce: [Route("api/secret")] ? ^^
+    public class SecretController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return SecretGet();
+        }
+
+        public string SecretGet()
+        {
+            return "This is the secret API";
+        }
+    }
+}
