14898ee4f742576f9fb2c51839999411be15c1e7
Chris Lawrence <chris.lawrence@lexisnexis.co.uk>
2018-11-07 09:49:58 +0000

    added scrambler interface

---
+++ IScrambler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretApi
+{
+    public interface IScrambler
+    {
+        string Scramble(string messageToBeScrambled);
+    }
+}
